f8999b8cd96417160d2a7211680336f4f4ea8e89
KienVT <votrungkien2000@gmail.com>
2022-11-11 14:54:54 +0700

    cào dc

--- BE/src/Utils/scraping.js
+++ BE/src/Utils/scraping.js
@@ -1,87 +1,99 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-const url = "https://www.booking.com/index.vi.html?aid=336510&label=melbourne-biF21fFWLuf31kP48lLVsAS154363258292%3Apl%3Ata%3Ap1%3Ap2260%2C000%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666515%3Akwd-40826943%3Alp9053233%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YXL5GV3cgz10NyjSyBn12N8&sid=a9f5c3e68e5e1879fe8b8ee80f3c5b2c&click_from_logo=1";
-const listAdress = []
-const listPage = []
-const linkToHotel = []
-const detailHotels = []
+// const url = "https://www.booking.com/index.vi.html?aid=336510&label=melbourne-biF21fFWLuf31kP48lLVsAS154363258292%3Apl%3Ata%3Ap1%3Ap2260%2C000%3Aac%3Aap%3Aneg%3Afi%3Atiaud-297601666515%3Akwd-40826943%3Alp9053233%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YXL5GV3cgz10NyjSyBn12N8&sid=a9f5c3e68e5e1879fe8b8ee80f3c5b2c&click_from_logo=1";
 
-
-const getData = async (url) => {
+const getData = async () => {
     try {
-        const response = await axios.get(url)
+        const response = await axios.get("https://www.bestprice.vn/khach-san/vinpearl-resort-spa-ha-long-1344.html")
         const $ = cheerio.load(response.data)
-        const getlink = $(".bui-carousel__item");
-        getlink.each(function () {
-            href = $(this).find(".popular-destinations-carousel-link").attr("href");
-            if (href !== undefined) {
-                listAdress.push({ href })
-            }
-        });
-        // for (const Adress of listAdress) {
-        //     await getPage(Adress.href)
-        // }
-        // console.log(listAdress)
-        return listAdress
+        const getlink = $(".field-item p").text();
+        console.log(getlink)
     } catch (error) {
         console.log(error)
     }
 }
+getData()
+// const listAdress = []
+// const listPage = []
+// const linkToHotel = []
+// const detailHotels = []
 
-const getLinkDetail = async () => {
-    try {
-        const listPageHotel = await getPage()
-        for (detailHotelPage of listPageHotel) {
-            const response = await axios.get(detailHotelPage)
-            const $ = cheerio.load(response.data)
-            const getLinkDetail = $(".a826ba81c4")
-            getLinkDetail.each(function () {
-                href = $(this).find(".a4225678b2 a").attr("href")
-                linkToHotel.push(href)
-            })
-        }
-        return linkToHotel;
-    } catch (error) {
-        console.log(error)
-    }
-}
-const getDetail = async ()=> {
-        try {
-            const listLink = await getLinkDetail()
-            for(link of listLink){
-                // console.log(link)
-                if(link !== undefined) {
-                    const response = await axios.get(link)
-                    const $ = cheerio.load(response.data)
-                    const hotel = $(".txp-bui-header-pp .bui-button__text").text();
-                    console.log(hotel)
-                    // hotel.each(function(){
-                    //     nameHotel = $(this).find(".page-section .d2fee87262").text()
-                    //     console.log(nameHotel)
-                    // })
-                }
-            }
-            return listLink
-        } catch (error) {
-            console.log(error)
-        }
-} 
-const getPage = async () => {
-    try {
-        const linkPage = await getData(url)
-        for (Data of linkPage) {
-            const response = await axios.get(Data.href)
-            const $ = cheerio.load(response.data)
-            // console.log(typeof parseInt($(".a8b500abde").children("li").last().children("button").text()))
-            for (let i = 1; i <= 2; i++) {
-                let nextPage = Data.href + `offset=${25 * i - 25}`;
-                listPage.push(nextPage)
-            }
-            return listPage
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-module.exports = getDetail
+// const getData = async (url) => {
+//     try {
+//         const response = await axios.get(url)
+//         const $ = cheerio.load(response.data)
+//         const getlink = $(".bui-carousel__item");
+//         getlink.each(function () {
+//             href = $(this).find(".popular-destinations-carousel-link").attr("href");
+//             if (href !== undefined) {
+//                 listAdress.push({ href })
+//             }
+//         });
+//         // for (const Adress of listAdress) {
+//         //     await getPage(Adress.href)
+//         // }
+//         // console.log(listAdress)
+//         return listAdress
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// const getLinkDetail = async () => {
+//     try {
+//         const listPageHotel = await getPage()
+//         for (detailHotelPage of listPageHotel) {
+//             const response = await axios.get(detailHotelPage)
+//             const $ = cheerio.load(response.data)
+//             const getLinkDetail = $(".a826ba81c4")
+//             getLinkDetail.each(function () {
+//                 href = $(this).find(".a4225678b2 a").attr("href")
+//                 linkToHotel.push(href)
+//             })
+//         }
+//         return linkToHotel;
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// const getDetail = async ()=> {
+//         try {
+//             const listLink = await getLinkDetail()
+//             for(link of listLink){
+//                 // console.log(link)
+//                 if(link !== undefined) {
+//                     const response = await axios.get(link)
+//                     const $ = cheerio.load(response.data)
+//                     const hotel = $(".d2fee87262.pp-header__title")
+//                     console.log(hotel)
+//                     // hotel.each(function(){
+//                     //     nameHotel = $(this).find(".page-section .d2fee87262").text()
+//                     //     console.log(nameHotel)
+//                     // })
+//                 }
+//             }
+//             return listLink
+//         } catch (error) {
+//             console.log(error)
+//         }
+// } 
+// const getPage = async () => {
+//     try {
+//         const linkPage = await getData(url)
+//         for (Data of linkPage) {
+//             const response = await axios.get(Data.href)
+//             const $ = cheerio.load(response.data)
+//             // console.log(typeof parseInt($(".a8b500abde").children("li").last().children("button").text()))
+//             for (let i = 1; i <= 2; i++) {
+//                 let nextPage = Data.href + `offset=${25 * i - 25}`;
+//                 listPage.push(nextPage)
+//             }
+//             return listPage
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// module.exports = getDetail
--- FE/src/component/card/Infomation.js
+++ FE/src/component/card/Infomation.js
@@ -1,10 +1,37 @@
+import 'asset/style/information.css'
+
 function Information() {
-    return(
-        <div style={{padding:"10px"}} className="infomation">
+    return (
+        <div style={{ padding: "10px" }} className="infomation">
             <h4>Sài gòn Hotel</h4>
-            <div style={{padding:"20px", border: "1px solid var(--shape)", borderRadius: "10px"}}>
-                <p>Hãy thư giãn tại khu spa phục vụ đầy đủ các liệu pháp - nơi quý vị có thể tận hưởng các dịch vụ massage, trị liệu toàn thân và chăm sóc da mặt. Chắc chắn quý vị sẽ yêu thích những dịch vụ, tiện nghi thể thao, giải trí, gồm hồ bơi ngoài trời, phòng tắm hơi và trung tâm thể dục. Các dịch vụ, tiện ích khác tại khách sạn này bao gồm quyền truy cập Internet không dây miễn phí, dịch vụ tư vấn/hỗ trợ khách và cửa hàng quà tặng/sạp báo.Khách có thể lựa chọn dùng bữa tại nhà hàng của khách sạn hoặc nghỉ lại trong phòng và thưởng thức những món ăn ngon với dịch vụ phòng 24 giờ. Hãy thư giãn cùng với ly nước giải khát yêu thích của mình tại quầy bar/khu lounge hoặc quầy bar bên hồ bơi. Khách có thể dùng bữa sáng đặc sản địa phương miễn phí hàng ngày từ 07:00 đến 10:00.Các khu vực sau đóng cửa từ 1 Tháng Bảy 2021 đến 1 Tháng Chín 2022 (thời gian có thể thay đổi): Khu ẩm thựcKhu phòng tập thể dụcCó nhiều dịch vụ, tiện ích dành cho khách, bao gồm máy vi tính, dịch vụ giặt ủi/giặt khô và quầy tiếp tân phục vụ 24 giờ/ngày. Xe đưa đón sân bay phục vụ 24 giờ với phụ phí, bên cạnh đó cũng có bãi đậu xe tự phục vụ miễn phí trong khuôn viên.Hãy nghỉ ngơi thoải mái như đang ở nhà mình tại một trong 49 phòng trang trí khác biệt với tủ lạnh và iPod dock. Các phòng được trang bị TV màn hình phẳng 43-inch với các chương trình truyền hình cáp để khách giải trí; trong khi đó, truy cập Internet không dây miễn phí sẽ giúp quý vị luôn kết nối. Phòng tắm riêng với bồn tắm và buồng tắm vòi sen riêng được trang bị bồn tắm và đồ dùng nhà tắm miễn phí. Các tiện nghi phòng bao gồm két bảo mật và bàn, cùng điện thoại với điện thoại nội vùng miễn phí.Khoảng cách được làm tròn đến 0,1 km (hoặc dặm). Bờ sông Hội An - 0,1 km Phố cổ Hội An - 0,2 km Nhà Trần Dương - 0,3 km Chợ vải Hội An - 0,5 km Hội quán Triều Châu - Chùa Ông Bổn - 0,5 km Hội quán Hải Nam - Quỳnh Phủ - 0,6 km Chợ Hội An - 0,7 km Chùa Ông (Miếu Quan Công) - 0,7 km Bảo tàng Văn hóa Dân gian - 0,7 km Hội quán Phúc Kiến - 0,8 km Hội quán Phúc Kiến - Chùa Kim An - 0,8 km Bảo tàng Lịch sử & Nghệ thuật - 0,8 km Bảo tàng Văn hóa dân gian - 0,9 km Nhà cổ Diệp Đồng Nguyên - 1 km Làng chài Thanh Nam - 1 km Sân bay gần nhất là Sân bay Đà Nẵng (DAD-Sân bay Quốc tế Đà Nẵng), cách khoảng 29,5 kmTọa lạc tại trung tâm Hội An, Little Riverside. A Luxury Hotel & Spa cách Bờ sông Hội An vài bước và cách Phố cổ Hội An 5 phút đi bộ. Khách sạn 5 sao này cách Chợ Hội An 0,5 mi (0,8 km) và cách Công viên Chủ đề Ấn tượng 
-                Hội An 0,8 mi (1,2 km).Ở Cẩm Châu, Hội An</p>
+            <div className="infomation__title">
+                <div style={{ padding: "20px", border: "1px solid var(--shape)", borderRadius: "10px" }}>
+                    <p>Hãy thư giãn tại khu spa phục vụ đầy đủ các liệu pháp - nơi quý vị có thể tận hưởng các dịch vụ massage, trị liệu toàn thân và chăm sóc da mặt. Chắc chắn quý vị sẽ yêu thích những dịch vụ, tiện nghi thể thao, giải trí, gồm hồ bơi ngoài trời, phòng tắm hơi và trung tâm thể dục. Các dịch vụ, tiện ích khác tại khách sạn này bao gồm quyền truy cập Internet không dây miễn phí, dịch vụ tư vấn/hỗ trợ khách và cửa hàng quà tặng/sạp báo.Khách có thể lựa chọn dùng bữa tại nhà hàng của khách sạn hoặc nghỉ lại trong phòng và thưởng thức những món ăn ngon với dịch vụ phòng 24 giờ. Hãy thư giãn cùng với ly nước giải khát yêu thích của mình tại quầy bar/khu lounge hoặc quầy bar bên hồ bơi. Khách có thể dùng bữa sáng đặc sản địa phương miễn phí hàng ngày từ 07:00 đến 10:00.Các khu vực sau đóng cửa từ 1 Tháng Bảy 2021 đến 1 Tháng Chín 2022 (thời gian có thể thay đổi): Khu ẩm thựcKhu phòng tập thể dụcCó nhiều dịch vụ, tiện ích dành cho khách, bao gồm máy vi tính, dịch vụ giặt ủi/giặt khô và quầy tiếp tân phục vụ 24 giờ/ngày. Xe đưa đón sân bay phục vụ 24 giờ với phụ phí, bên cạnh đó cũng có bãi đậu xe tự phục vụ miễn phí trong khuôn viên.Hãy nghỉ ngơi thoải mái như đang ở nhà mình tại một trong 49 phòng trang trí khác biệt với tủ lạnh và iPod dock. Các phòng được trang bị TV màn hình phẳng 43-inch với các chương trình truyền hình cáp để khách giải trí; trong khi đó, truy cập Internet không dây miễn phí sẽ giúp quý vị luôn kết nối. Phòng tắm riêng với bồn tắm và buồng tắm vòi sen riêng được trang bị bồn tắm và đồ dùng nhà tắm miễn phí. Các tiện nghi phòng bao gồm két bảo mật và bàn, cùng điện thoại với điện thoại nội vùng miễn phí.Khoảng cách được làm tròn đến 0,1 km (hoặc dặm). Bờ sông Hội An - 0,1 km Phố cổ Hội An - 0,2 km Nhà Trần Dương - 0,3 km Chợ vải Hội An - 0,5 km Hội quán Triều Châu - Chùa Ông Bổn - 0,5 km Hội quán Hải Nam - Quỳnh Phủ - 0,6 km Chợ Hội An - 0,7 km Chùa Ông (Miếu Quan Công) - 0,7 km Bảo tàng Văn hóa Dân gian - 0,7 km Hội quán Phúc Kiến - 0,8 km Hội quán Phúc Kiến - Chùa Kim An - 0,8 km Bảo tàng Lịch sử & Nghệ thuật - 0,8 km Bảo tàng Văn hóa dân gian - 0,9 km Nhà cổ Diệp Đồng Nguyên - 1 km Làng chài Thanh Nam - 1 km Sân bay gần nhất là Sân bay Đà Nẵng (DAD-Sân bay Quốc tế Đà Nẵng), cách khoảng 29,5 kmTọa lạc tại trung tâm Hội An, Little Riverside. A Luxury Hotel & Spa cách Bờ sông Hội An vài bước và cách Phố cổ Hội An 5 phút đi bộ. Khách sạn 5 sao này cách Chợ Hội An 0,5 mi (0,8 km) và cách Công viên Chủ đề Ấn tượng
+                        Hội An 0,8 mi (1,2 km).Ở Cẩm Châu, Hội An</p>
+                </div>
+            </div>
+            <div className="infomation__service">
+                <ul className="infomation__service__list">
+                    <li className="infomation__service__list__item">Wi-fi miễn phí</li>
+                    <li className="infomation__service__list__item">Đỗ xe miễn phí</li>
+                    <li className="infomation__service__list__item">Phù hợp cho người khuyết tật</li>
+                    <li className="infomation__service__list__item">Bể bơi</li>
+                    <li className="infomation__service__list__item">Có đường ra bãi biển</li>
+                </ul>
+                <ul className="infomation__service__list">
+                    <li className="infomation__service__list__item">Xe đưa đón ra sân bay</li>
+                    <li className="infomation__service__list__item">Không hút thuốc</li>
+                    <li className="infomation__service__list__item">Chấp nhận vật nuôi</li>
+                    <li className="infomation__service__list__item">Thang máy</li>
+                    <li className="infomation__service__list__item">Cầu là quần áo</li>
+                </ul>
+                <ul className="infomation__service__list">
+                    <li className="infomation__service__list__item">Xe đưa đón ra sân bay</li>
+                    <li className="infomation__service__list__item">Không hút thuốc</li>
+                    <li className="infomation__service__list__item">Chấp nhận vật nuôi</li>
+                    <li className="infomation__service__list__item">Thang máy</li>
+                    <li className="infomation__service__list__item">Cầu là quần áo</li>
+                </ul>
             </div>
         </div>
     )
